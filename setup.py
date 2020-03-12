5b01891960479e441b20783479eb63b6dfd57fd7
Matija Loncar <none@none.local>
2020-03-12 14:23:17 +0100

    version bump to 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='sendbee_api',
-    version='0.15.1',
+    version='1.0.0',
 
     description='Python client SDK for Sendbee Public API',
     long_description=readme(),
