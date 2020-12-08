7aee8d8ad329d8b4327790a5e8dbd2d123914750
Ivan Arar <ivan.arar@gmail.com>
2020-12-08 15:43:31 +0100

    hotfix/ujson-version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='sendbee_api',
-    version='1.5.0',
+    version='1.5.1',
 
     description='Python client SDK for Sendbee Public API',
     long_description=readme(),
@@ -37,7 +37,7 @@ setup(
         'requests>=2.20.0',
         'dumpit>=0.5.0',
         'aenum>=2.1.2',
-        'ujson==1.35',
+        'ujson==2.0.1',
         'cryptography==2.8'
     ],
 
